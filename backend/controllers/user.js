5685485f0cd47c2105ffe0add83147a26980e48b
Gabriel <gabrieldubois.eng@gmail.com>
2023-01-03 12:39:32 -0500

    profile update backend

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,9 @@
 const User = require('../models/user');
 const Course = require('../models/course');
 const { errorHandler } = require('../helpers/dbErrorHandler')
+const _ = require('lodash');
+const formidable = require('formidable');
+const fs = require('fs');
 
 exports.read = (req, res) => {
     req.profile.hashed_password = undefined;
@@ -39,5 +42,62 @@ exports.publicProfile = (req, res) => {
                     courses: data
                 });
             });
+    });
+};
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtension = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Photo could not be uploaded'
+            });
+        }
+        let user = req.profile;
+        user = _.extend(user, fields);
+
+        if (fields.password && fields.password.length < 6) {
+            return res.status(400).json({
+                error: 'Password should be min 6 characters long'
+            });
+        }
+
+        if (files.photo) {
+            if (files.photo.size > 10000000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1mb'
+                });
+            }
+            user.photo.data = fs.readFileSync(files.photo.path);
+            user.photo.contentType = files.photo.type;
+        }
+
+        user.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: 'All filds required'
+                });
+            }
+            user.hashed_password = undefined;
+            user.salt = undefined;
+            user.photo = undefined;
+            res.json(user);
+        });
+    });
+};
+
+exports.photo = (req, res) => {
+    const username = req.params.username;
+    User.findOne({ username }).exec((err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User not found'
+            });
+        }
+        if (user.photo.data) {
+            res.set('Content-Type', user.photo.contentType);
+            return res.send(user.photo.data);
+        }
     });
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { requireSignIn, authMiddleware, adminMiddleware } = require('../controllers/auth');
-const { publicProfile } = require('../controllers/user')
-const { read } = require('../controllers/user');
+const { read, publicProfile, update, photo } = require('../controllers/user');
 
 router.get('/profile', requireSignIn, authMiddleware, read);
 router.get('/user/:username', publicProfile);
+router.put('/user/update', requireSignIn, authMiddleware, update);
+router.get('/user/photo/:username', photo);
 
 module.exports = router;
